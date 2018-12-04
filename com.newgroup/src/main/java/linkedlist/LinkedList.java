e80a4fd31fc21b893d7c0c375b6c1700b65ce391
Manaye <hiwotmanaye2@gmail.com>
2018-12-04 13:05:59 -0800

    first linked list

--- com.newgroup/src/main/java/linkedlist/LinkedList.java
+++ com.newgroup/src/main/java/linkedlist/LinkedList.java
@@ -1,5 +1,47 @@
 package linkedlist;
 
+import java.util.ArrayList;
+
 public class LinkedList {
-//    append
+
+    private Node head;
+
+    public LinkedList() {
+        this.head = null;
+    }
+    public void insert(int value) {
+        Node newNode = new Node(value, this.head);
+        this.head = newNode;
+    }
+
+    public boolean includes(int value) {
+
+        Node current = this.head;
+
+        while (current != null) {
+            if (value == current.data) {
+                return true;
+            } else {
+                current = current.next;
+            }
+        }
+        return false;
+    }
+
+    public ArrayList<Integer> print() {
+        Node current = this.head;
+
+        ArrayList<Integer> printArr = new ArrayList<>();
+        while (current != null) {
+            printArr.add(current.data);
+            current = current.next;
+        }
+        System.out.println(printArr.toString());
+        return printArr;
+    }
 }
+
+
+
+
+
