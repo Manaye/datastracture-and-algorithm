7c9058b53ec267df3d95e40b3e18b68473715e11
Manaye <hiwotmanaye2@gmail.com>
2018-12-10 01:18:38 -0800

    change readme

--- README.md
+++ README.md
@@ -1,11 +1,16 @@
-# Binary Search
-<!-- Short summary or background information -->
+# Linked List Insertions
+
+Appending at the end and inserting before and inserting after a given value in linkedList.
 
 ## Challenge
-<!-- Description of the challenge -->
+1. Append to your linked list class that takes value and add at the end of the linkedList.
+1. Insertion before to your linkedList class that takes a value and new value.
+1. Insertion after method to your linkedList class that takes in a value and new value.
 
 ## Approach & Efficiency
-<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
-
+It requires to traverese the linkedList until we find the last node we do that in while loop and until we find that the last node next is pointing to null value and we then insert a new node.
+And all this method takes O(n) time and O(1) space
 ## Solution
-<!-- Embedded whiteboard image -->
+
+![](./asset/ll_insertion.jpg)
+![](./asset/ll_insertion2.jpg)
--- com.newgroup/src/test/java/linkedlist/LinkedListTest.java
+++ com.newgroup/src/test/java/linkedlist/LinkedListTest.java
@@ -7,78 +7,55 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class LinkedListTest {
-
+    LinkedList test = new LinkedList();
     @Test
     public void testinsert() {
         LinkedList oneList = new LinkedList();
         oneList.insert(3);
         oneList.insert(10);
         oneList.insert(9);
-
         LinkedList twoList = new LinkedList();
         twoList.insert(0);
-
-        assertTrue("expect true for oneList,3" ,oneList.includes(3));
-        assertFalse("expect true for oneList,0" ,oneList.includes(0));
+        assertTrue("expect true for oneList,3", oneList.includes(3));
+        assertFalse("expect true for oneList,0", oneList.includes(0));
     }
-
     @Test
     public void includes() {
         LinkedList onelist = new LinkedList();
         onelist.insert(6);
         onelist.insert(16);
-        onelist.insert(26);
-
         assertTrue("expect true for oneList,6" ,onelist.includes(6));
-        assertFalse("expect true for oneList,0" ,onelist.includes(0));
-        ArrayList<Integer> one = new ArrayList<>();
-        one.add(3);
-        one.add(5);
-        one.add(6);
-
-
-        LinkedList twoList = new LinkedList();
-        twoList.insert(0);
-        ArrayList<Integer> two = new ArrayList<>();
-        two.add(0);
-
-
-
-        LinkedList threelist = new LinkedList();
-        threelist.insert(2);
-        threelist.insert(2);
-        threelist.insert(2);
-
-        ArrayList<Integer>three = new ArrayList<>();
-        three.add(2);
-        three.add(2);
-        three.add(2);
-
-        assertEquals(one,onelist.print());
-        assertEquals(two,onelist.print());
-        assertEquals(two,onelist.print());
-
+        assertFalse("expect true for oneList,11" ,onelist.includes(11));
     }
-
     @Test
     public void testprint() {
         LinkedList oneList = new LinkedList();
         oneList.insert(3);
         oneList.insert(10);
-
-        ArrayList<Integer> one = new ArrayList<>();
-        one.add(3);
-        one.add(5);
-        one.add(6);
-
-        LinkedList twoList = new LinkedList();
-        twoList.insert(0);
-
-        ArrayList<Integer> two = new ArrayList<>();
-        two.add(0);
-
-        assertEquals(one,oneList.print());
-        assertEquals(two,oneList.print());
-
+        ArrayList<Integer> one = oneList.print();
+        assertTrue(one.size()>0);
     }
+    @Test
+    public void testAppend(){
+        test.insert(23);
+        test.append(34);
+        assertTrue(test.includes(34));
+    }
+    @Test
+    public void testInsertBefore(){
+        test.insert(50);
+        test.insert(100);
+        test.insertBefore(50,70);
+        assertTrue(test.includes(50));
+    }
+    @Test
+    public void testInsertAfter(){
+        test.insert(90);
+        test.insertAfter(90,95);
+        assertTrue(test.includes(90));
+    }
+
 }
+
+
+
